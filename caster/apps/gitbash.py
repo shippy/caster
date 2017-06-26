b097746ed5ab4ff857b62c1a2624fc8be27b8086
Simon Podhajsky <simon.podhajsky@gmail.com>
2017-06-26 15:51:50 -0400

    Added context, commands to git bash + CCR for Matlab, R

--- caster/apps/gitbash.py
+++ caster/apps/gitbash.py
@@ -27,55 +27,65 @@ class GitBashRule(MergeRule):
     pronunciation = "git bash"
 
     mapping = {
-        "initialize repository":       Text( "git init" )+Key("enter"),
-        "add":              R(Key("g, i, t, space, a, d, d, space, dot, enter"), rdescript="GIT: Add All"),
-        "status":           R(Key( "g, i, t, space, s, t, a, t, u, s, enter" ), rdescript="GIT: Status"),
-        "commit":           R(Key( "g, i, t, space, c, o, m, m, i, t, space, minus, a, m, space, apostrophe, apostrophe, left"), rdescript="GIT: Commit"),
+        "initialize repository":       Text( "git init" ) + Key("enter"),
+        "add all":              R(Text('git add -A') + Key('enter'), rdescript="GIT: Add All"),
+        "add [some]":              R(Text('git add '), rdescript="GIT: Add some"),
+        "patch":              R(Text('git add -Ap') + Key('enter'), rdescript="GIT: Add by hunks"),
+        "status":           R(Text("git status") + Key("enter"), rdescript="GIT: Status"),
+        "show":           R(Text("git show") + Key("enter"), rdescript="GIT: Show last commit's diff"),
+        "diff | difference":           R(Text("git diff") + Key("enter"), rdescript="GIT: Diff"),
+        "preview [commit]":           R(Text("git diff --cached") + Key("enter"), rdescript="GIT: Preview commit diff"),
+        "commit":           R(Text("git commit") + Key("enter"), rdescript="GIT: Commit"),
         "bug fix commit <n>":    R(Mimic("commit")+Text("fixes #%(n)d ")+Key("backspace"), rdescript="GIT: Bug Fix Commit"),
         "reference commit <n>":  R(Mimic("commit")+Text("refs #%(n)d ")+Key("backspace"), rdescript="GIT: Reference Commit"),
+        "amend":  R(Text("git commit --amend")+Key("enter"), rdescript="GIT: Amend Commit"),
         "checkout":         R(Text( "git checkout " ), rdescript="GIT: Check Out"),
-        "branch":         R(Text( "git branch" )+Key("enter"), rdescript="GIT: Branch"),
-        "remote":         R(Text( "git remote " ), rdescript="GIT: Remote"),
-        "merge":            R(Text( "git merge " ), rdescript="GIT: Merge"),
-        "merge tool":       R(Text( "git mergetool")+Key("enter"), rdescript="GIT: Merge Tool"),
-        "fetch":            R(Text( "git fetch" )+Key("enter"), rdescript="GIT: Fetch"),
-        
-        
+        "branch":           R(Text("git branch -vv") + Key("enter"), rdescript="GIT: Branch"),
+        "remote":           R(Text("git remote -vv"), rdescript="GIT: Remote"),
+        "merge":            R(Text("git merge "), rdescript="GIT: Merge"),
+        "merge tool":       R(Text("git mergetool")+Key("enter"), rdescript="GIT: Merge Tool"),
+        "fetch":            R(Text("git fetch")+Key("enter"), rdescript="GIT: Fetch"),
+        "log":              R(Text("git log --stat --all")+Key("enter"), rdescript="GIT: Log"),
+        "short log":        R(Text("git log --graph --abbrev-commit --all")+Key("enter"), rdescript="GIT: One-line log"),
+
+
         "(get push | push)":R(Text( "git push" )+Key("enter"), rdescript="GIT: Push"),
         "pull":             R(Text( "git pull" )+Key("enter"), rdescript="GIT: Pull"),
+        "pull with rebase":             R(Text( "git pull --rebase" )+Key("enter"), rdescript="GIT: Pull"),
         "CD up":            R(Text( "cd .." )+Key("enter"), rdescript="GIT: Up Directory"),
         "CD":               R(Text( "cd " ), rdescript="GIT: Navigate Directory"),
         "list":             R(Text( "ls" )+Key("enter"), rdescript="GIT: List"),
         "make directory":   R(Text( "mkdir " ), rdescript="GIT: Make Directory"),
-        
-        
-        
+
+
+
         "undo [last] commit":       R(Text("git reset --soft HEAD~1")+Key("enter"), rdescript="GIT: Undo Commit"),
         "(undo changes | reset hard)": R(Text("git reset --hard")+Key("enter"), rdescript="GIT: Undo or Reset Since Last Commit"),
         "stop tracking [file]":     R(Text("git rm --cached FILENAME"), rdescript="GIT: Stop Tracking"),
         "preview remove untracked": R(Text("git clean -nd")+Key("enter"), rdescript="GIT: Preview Remove Untracked"),
         "remove untracked":         R(Text("git clean -fd")+Key("enter"), rdescript="GIT: Remove Untracked"),
-        
+
         "visualize":        R(Text("gitk")+Key("enter"), rdescript="GIT: gitk"),
         "visualize file":   R(Text("gitk -- PATH"), rdescript="GIT: gitk Single File"),
         "visualize all":    R(Text("gitk --all")+Key("enter"), rdescript="GIT: gitk All Branches"),
-        
+
         "exit":             R(Text( "exit" )+Key("enter"), rdescript="GIT: Exit"),
-        
-        
-        
+
+
+
         "stash":            R(Text("git stash")+Key("enter"), rdescript="GIT: Stash"),
+        "stash pop | restore": R(Text("git stash pop")+Key("enter"), rdescript="GIT: Stash"),
         "stash apply [<n>]":R(Text("git stash apply")+Function(_apply), rdescript="GIT: Stash Apply"),
         "stash list":       R(Text("git stash list")+Key("enter"), rdescript="GIT: Stash List"),
         "stash branch":     R(Text("git stash branch NAME"), rdescript="GIT: Stash Branch"),
 
         "cherry pick":      R(Text("git cherry-pick "), rdescript="GIT: Cherry Pick"),
         "abort cherry pick":R(Text("git cherry-pick --abort"), rdescript="GIT: Abort Cherry Pick"),
-        
+
         "GUI | gooey":      R(Text("git gui")+Key("enter"), rdescript="GIT: gui"),
         "blame":            R(Text("git blame PATH -L FIRSTLINE,LASTLINE"), rdescript="GIT: Blame"),
         "gooey blame":      R(Text("git gui blame PATH"), rdescript="GIT: GUI Blame"),
-        
+
         "search recursive": R(Text("grep -rinH \"PATTERN\" *"), rdescript="GREP: Search Recursive"),
         "search recursive count": R(Text("grep -rinH \"PATTERN\" * | wc -l"), rdescript="GREP: Search Recursive Count"),
         "search recursive filetype": R(Text("find . -name \"*.java\" -exec grep -rinH \"PATTERN\" {} \\;"), rdescript="GREP: Search Recursive Filetype"),
@@ -93,8 +103,10 @@ context = AppContext(executable="\\sh.exe")
 context2 = AppContext(executable="\\bash.exe")
 context3 = AppContext(executable="\\cmd.exe")
 context4 = AppContext(executable="\\mintty.exe")
+context5 = AppContext(executable="\\ConEmu64.exe")
+context6 = AppContext(executable="\\PUTTY.EXE")
 
-grammar = Grammar("MINGW32", context=(context | context2 | context3 | context4))
+grammar = Grammar("MINGW32", context=(context | context2 | context3 | context4 | context5 | context6))
 
 if settings.SETTINGS["apps"]["gitbash"]:
     if settings.SETTINGS["miscellaneous"]["rdp_mode"]:
